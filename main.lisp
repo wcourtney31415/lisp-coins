023ff0b4dfb637cae8364ddc72c8ef996116de35
wcourtney31415 <wcourtney31415@gmail.com>
2021-09-13 02:25:01 -0400

    Initial Commit.

---
+++ main.lisp
@@ -0,0 +1,55 @@
+
+( print "Enter a dollar ammount as a decimal number:" )
+( setq ammount 
+    ( read ) )
+( defun runGoStart 
+    ( ammount myList coinList ) 
+    ( 
+if
+coinList
+        ( 
+let*
+            ( 
+                ( coin 
+                    ( first coinList ) ) 
+                ( evenFit 
+                    ( floor 
+                        ( / ammount coin ) ) ) 
+                ( nextAmmount 
+                    ( - ammount 
+                        ( * evenFit coin ) ) ) 
+                ( nextList 
+                    ( cons evenFit myList ) ) 
+                ( nextCoinList 
+                    ( cdr coinList ) ) ) 
+            (  runGoStart nextAmmount nextList nextCoinList  )
+)        (reverse myList)))
+
+(defun fancyPrint 
+    (myList myString coinNames) 
+    (
+if
+        myList
+        ( 
+let*
+            (
+                (coinName (first coinNames))
+                (nextCoinNames (cdr coinNames))
+                (myText (concatenate 'string (write-to-string (first myList)) coinName))
+                (nextString 
+                    (concatenate 'string myString 
+                        myText)) 
+                (nextList 
+                    (cdr myList)))
+            (
+fancyPrint nextList nextString nextCoinNames
+)
+)myString
+))
+
+( print 
+    (fancyPrint 
+        ( runGoStart 
+            ( * ammount 100 ) Nil 
+            ( List 25 10 5 1 )  ) "" (List " Quarters " " Dimes " " Nickles " " Pennies ")
+))
